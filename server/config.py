03fec3dcf72f3ee192eed1440488694f2fc2f6bb
Ian Scott Martin <ianscottmartin@gmail.com>
2023-09-26 10:49:07 -0600

    new commit

---
+++ server/config.py
@@ -0,0 +1,24 @@
+# config.py
+from flask import Flask
+from flask_bcrypt import Bcrypt
+from flask_migrate import Migrate
+from  flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+
+app= Flask(__name__)
+app.secret_key = b'\x14\xa6\xc8\xa3\\\x87\x9d8k\xf0\x19A$q\xdf\x97'
+app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+db = SQLAlchemy()
+
+migrate = Migrate(app, db)
+db.init_app(app)
+
+bcrypt =Bcrypt(app)
+
+api = Api(app)
+
+pi(app)
+
